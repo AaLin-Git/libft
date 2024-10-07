0aef9c80eb73f4421d9996a9f0baa3a32cdd7269
Alona Kovalchuk <akovalch@f0r5s10.codam.nl>
2024-10-07 13:46:25 +0200

    created files

---
+++ ft_atoi.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akovalch <akovalch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 13:45:42 by akovalch          #+#    #+#             */
+/*   Updated: 2024/10/07 13:45:53 by akovalch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	inner_loop(char *str, int *num, int *negative)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] == ' ' || str[i] == '+' || (str[i] >= 9 && str[i] <= 13))
+	{
+		i++;
+	}
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			*negative *= -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		*num = *num * 10 + (str[i] - '0');
+		i++;
+	}
+}
+
+int	ft_atoi(char *str)
+{
+	int	num;
+	int	negative;
+
+	num = 0;
+	negative = 1;
+	inner_loop(str, &num, &negative);
+	num *= negative;
+	return (num);
+}
