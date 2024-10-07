c90bc9d9e0acadd3120d382ca2c21fc167a680b1
Alona Kovalchuk <akovalch@f0r5s10.codam.nl>
2024-10-07 14:40:39 +0200

    added more functions

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,23 +1,45 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: akovalch <akovalch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/07 14:21:10 by akovalch          #+#    #+#             */
-/*   Updated: 2024/10/07 14:25:45 by akovalch         ###   ########.fr       */
+/*   Created: 2024/10/07 14:20:28 by akovalch          #+#    #+#             */
+/*   Updated: 2024/10/07 14:20:45 by akovalch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*strchr(const char *s, int c)
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
 {
 	int	i;
 
 	i = 0;
-	if (s[i] == c)
+	while (str[i] != '\0')
 	{
-		return (s[i]);
+		i++;
 	}
-	return (0);
+	return (i);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	str = malloc((ft_strlen(src) + 1) * sizeof(char));
+	if (str == NULL)
+	{
+		return (0);
+	}
+	while (src[i] != '\0')
+	{
+		str[i] = src[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
---
+++ ft_strlcpy.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akovalch <akovalch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/07 14:32:15 by akovalch          #+#    #+#             */
+/*   Updated: 2024/10/07 14:40:15 by akovalch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	str_length(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+unsigned	int	ft_strlcpy(char *dest, char *src, unsigned int size)
+{
+	unsigned int	dest_length;
+	unsigned int	src_length;
+	unsigned int	i;
+
+	dest_length = str_length(dest);
+	src_length = str_length(src);
+	i = 0;
+	if (size < 0)
+	{
+		return (size + src_length);
+	}
+	while (src[i] != '\0' && i < size - dest_length - 1)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (src_length);
+}
