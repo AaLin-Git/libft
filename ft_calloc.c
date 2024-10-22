1528a7473ef9f7cd1c373cd37bf58d037b9092a9
Alona Kovalchuk <akovalch@f0r4s22.codam.nl>
2024-10-22 17:40:09 +0200

    add main

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: akovalch <akovalch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 15:05:49 by akovalch          #+#    #+#             */
-/*   Updated: 2024/10/22 17:09:39 by akovalch         ###   ########.fr       */
+/*   Updated: 2024/10/22 17:35:34 by akovalch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,3 +27,30 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	return (arr);
 }
 
+/* int	main(void)
+{
+	int		*arr;
+	size_t	i = 0;
+	size_t	nmemb = 5;
+	size_t	size = sizeof(int);
+
+	arr = (int *)ft_calloc(nmemb, size);
+	if (arr == NULL)
+	{
+		printf("Error, can't allocate the memory\n");
+		return (1);
+	}
+	while (i < nmemb)
+	{
+		if (arr[i] != 0)
+		{
+			printf("Error\n");
+			free(arr);
+			return (1);
+		}
+		i++;
+	}
+	printf("It works\n");
+	free(arr);
+	return (0);
+} */
